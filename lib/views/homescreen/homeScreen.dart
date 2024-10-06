5ef886d9612f12b79f54c7c97b81d6b84d219e8a
Ankan Chandra Biswas <ankan.dev.prog@gmail.com>
2024-10-07 00:32:56 +0600

    update

--- lib/views/homescreen/homeScreen.dart
+++ lib/views/homescreen/homeScreen.dart
@@ -15,7 +15,20 @@ class _HomescreenState extends State<Homescreen> {
       appBar: AppBar(
         backgroundColor: AppColor.homepageappbarcolor,
         actions: [
-          CircleAvatar(child: IconButton(onPressed: (){}, icon: Icon(Icons.person,color: AppColor.whiteColor,)))
+          IconButton(onPressed: (){}, icon: Container(
+            // width: 45, // ছবির প্রস্থ
+            // height: 45, // ছবির উচ্চতা
+            // decoration: BoxDecoration(
+            //   shape: BoxShape.circle,
+            //   border: Border.all(color: Colors.white, width: 0.0), // বর্ডার কালার এবং পুরুত্ব
+            // ),
+            child: CircleAvatar(
+              backgroundColor: AppColor.whiteColor,
+              child: ClipOval( // ছবি ক্লিপ করার জন্য
+                child: Image.asset('assets/homepage/1-appbarprofile.png',fit: BoxFit.fill,),
+              ),
+            ),
+          ))
         ],
       ),
     );
