8f36bd4e372f0f6951f48ab0ab6269bda61b0ba4
Ankan Chandra Biswas <ankan.dev.prog@gmail.com>
2024-10-22 11:57:04 +0600

    json

--- lib/controller/homeController.dart
+++ lib/controller/homeController.dart
@@ -2,4 +2,50 @@ import 'package:get/get.dart';
 
 class HomeContrller extends GetxController{
 
+  List HomeGridview = [
+    {
+      'name' : 'Admission\nand  Fee',
+      'img' : '3-admission and fee.png',
+    },
+    {
+      'name' : 'Department',
+      'img' : '4-department.png',
+    },
+    {
+      'name' : 'Notice Board',
+      'img' : '5-noticeboard.png',
+    },
+    {
+      'name' : 'Teachers\nInformation',
+      'img' : '6-teachers information.png',
+    },
+    {
+      'name' : 'Result',
+      'img' : '7-result.jpg',
+    },
+    {
+      'name' : 'Student of the\nAward',
+      'img' : '8-student of the award.png',
+    },
+    {
+      'name' : 'Skill Development',
+      'img' : '9-skill development.png',
+    },
+    {
+      'name' : 'Entertainment',
+      'img' : '10-entertainment.png',
+    },
+    {
+      'name' : 'PPI Alumni\nAssociation',
+      'img' : '11-ppi alumni association.png',
+    },
+    {
+      'name' : 'PPI Career',
+      'img' : '12-ppi career.png',
+    },
+    {
+      'name' : 'About institute',
+      'img' : '13-about institute.png',
+    },
+  ];
 }
--- lib/views/homescreen/home_all_itemgriview.dart
+++ lib/views/homescreen/home_all_itemgriview.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:ppi/controller/homeController.dart';
 
 class HomeAllItemgriview extends StatefulWidget {
   const HomeAllItemgriview({super.key});
@@ -10,83 +12,67 @@ class HomeAllItemgriview extends StatefulWidget {
 class _HomeAllItemgriviewState extends State<HomeAllItemgriview> {
   @override
   Widget build(BuildContext context) {
-    return  Transform.translate(
-      offset: Offset(0, -180),  // X এবং Y এ স্থানান্তর
-      child: GridView.builder(
-        itemCount: 20,
-        physics: NeverScrollableScrollPhysics(),  // GridView scroll হবে না
-        shrinkWrap: true,  // Content wrap করে ফেলা হবে
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,  // 2 টা column
-          crossAxisSpacing: 10,  // Column এর মধ্যে spacing
-          mainAxisSpacing: 10,  // Row এর মধ্যে spacing
-          childAspectRatio: 1,  // Aspect ratio ঠিক রাখতে হবে (height:width = 1:1)
-        ),
-        itemBuilder: (BuildContext context, int index) {
-          return Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Card(
-              elevation: 5,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                ), // Container এর background color
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    // ClipRRect(
-                    //     borderRadius: BorderRadius.circular(100),  // 20px এর গোলাকার কোণা
-                    //     child: Container(
-                    //       height: 80,  // Container এর উচ্চতা
-                    //       width: 80,   // Container এর প্রস্থ
-                    //       decoration: BoxDecoration(
-                    //         color: Colors.white,
-                    //         borderRadius: BorderRadius.all(Radius.circular(100)),
-                    //         boxShadow: [
-                    //           BoxShadow(
-                    //             color: Colors.black.withOpacity(1),
-                    //             spreadRadius: 5,
-                    //             blurRadius: 7,
-                    //             offset: Offset(0, 3), // changes position of shadow
-                    //           ),
-                    //         ],
-                    //       ),
-                    //       child: Center(
-                    //         child: Image.asset('assets/homepage/1-appbarprofile.png',fit: BoxFit.fill,)
-                    //       ),
-                    //     ),
-                    // ),
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.2),  // Shadow এর রঙ
-                            spreadRadius: 2,  // Shadow এর বিস্তার
-                            blurRadius: 16,   // Blur এর পরিমাণ
-                            offset: Offset(0, 5),  // Shadow এর অবস্থান (X,Y)
+    return GetBuilder<HomeContrller>(builder: (HomeContrller homcon) {
+      return Transform.translate(
+        offset: Offset(0, -180),  // X এবং Y এ স্থানান্তর
+        child: GridView.builder(
+          itemCount: homcon.HomeGridview.length,
+          physics: NeverScrollableScrollPhysics(),  // GridView scroll হবে না
+          shrinkWrap: true,  // Content wrap করে ফেলা হবে
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,  // 2 টা column
+            crossAxisSpacing: 10,  // Column এর মধ্যে spacing
+            mainAxisSpacing: 10,  // Row এর মধ্যে spacing
+            childAspectRatio: 1,  // Aspect ratio ঠিক রাখতে হবে (height:width = 1:1)
+          ),
+          itemBuilder: (BuildContext context, int index) {
+            print(homcon.HomeGridview[index]);
+
+            var homegridviewitems = homcon.HomeGridview[index];
+
+            return Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Card(
+                elevation: 5,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                  ), // Container এর background color
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black.withOpacity(0.2),  // Shadow এর রঙ
+                              spreadRadius: 2,  // Shadow এর বিস্তার
+                              blurRadius: 16,   // Blur এর পরিমাণ
+                              offset: Offset(0, 5),  // Shadow এর অবস্থান (X,Y)
+                            ),
+                          ],
+                        ),
+                        child: CircleAvatar(
+                          radius: 40,
+                          backgroundColor: Colors.white,
+                          child: ClipOval(
+                              child: Image.asset('assets/homepage/1-appbarprofile.png',fit: BoxFit.fill,)
                           ),
-                        ],
-                      ),
-                      child: CircleAvatar(
-                        radius: 40,
-                        backgroundColor: Colors.white,
-                        child: ClipOval(
-                            child: Image.asset('assets/homepage/1-appbarprofile.png',fit: BoxFit.fill,)
                         ),
                       ),
-                    ),
 
 
-                    Text('data')
-                  ],
+                      Text('${homegridviewitems}'),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          );
-        },
-      ),
-    );
+            );
+          },
+        ),
+      );
+    },);
   }
 }
