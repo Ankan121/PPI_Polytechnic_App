7e95bf25276743e8d5bb48930028d853df092633
Ankan Chandra Biswas <ankan.dev.prog@gmail.com>
2024-11-07 13:51:30 +0600

    ppi alumni

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -5,5 +5,6 @@ class AppColor{
   static Color? homepageappbarcolor = Color(0xffF5BC51);
   static Color? buttoncolor = Color(0xffF1C572);
   static Color? textfromfiledcolor = Color(0xffF5CA7A);
+  static Color? ppialumniview = Color(0xffF5CA7A);
   static Color? whiteColor = const Color(0xffFFFFFF);
 }
--- lib/views/homescreen/HomeGridViewItems/PPIAlumniassociation/ppialumniallview.dart
+++ lib/views/homescreen/HomeGridViewItems/PPIAlumniassociation/ppialumniallview.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:ppi/constants/color.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../constants/customtext.dart';
 import '../../../../controller/homeController.dart';
@@ -23,6 +24,33 @@ class _PPIAlumniAllViewState extends State<PPIAlumniAllView> {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       Get.find<HomeContrller>().setppialumnimodel();
     });
+  }
+
+  // ফোন ডায়ালার খোলার ফাংশন
+  void _launchDialPad() async {
+    final Uri phoneUri = Uri(
+      scheme: 'tel',
+      path: widget.ppialumnimodel?.phone,
+    );
+    if (await canLaunchUrl(phoneUri)) {
+      await launchUrl(phoneUri);
+    } else {
+      throw 'Could not launch ${widget.ppialumnimodel?.phone}';
+    }
+  }
+
+  // ইমেইল পাঠানোর ফাংশন
+  void _sendEmail() async {
+    final Uri emailUri = Uri(
+      scheme: 'mailto',
+      path: widget.ppialumnimodel?.email ?? 'example@gmail.com',
+      query: 'subject=Hello&body=I would like to contact you', // ঐচ্ছিক
+    );
+    if (await canLaunchUrl(emailUri)) {
+      await launchUrl(emailUri);
+    } else {
+      throw 'Could not send email to ${widget.ppialumnimodel?.email}';
+    }
   }
 
 
@@ -78,74 +106,226 @@ class _PPIAlumniAllViewState extends State<PPIAlumniAllView> {
         body: SingleChildScrollView(
           child: Column(
             children: [
-              SizedBox(height: 50.h,),
-              Container(
-                height: 600.h,
-                width: double.infinity.w,
-                decoration: BoxDecoration(
-                  color: AppColor.whiteColor,
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(40.0),
-                    topLeft: Radius.circular(40.0),
-                  ),
-                ),
-                child:  SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        SizedBox(height: 10.h,),
-                        Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment: MainAxisAlignment.center,
+              SizedBox(height: 100.h,),
+              Stack(
+                clipBehavior: Clip.none, // Overflow অংশ কাটবে না
+                children: [
+                  Container(
+                      height: 600.h,
+                      width: double.infinity.w,
+                      decoration: BoxDecoration(
+                        color: AppColor.whiteColor,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(40.0),
+                          topLeft: Radius.circular(40.0),
+                        ),
+                      ),
+                      child:  SingleChildScrollView(
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              SizedBox(height: 50.h,),
+                              Column(
                                 children: [
-                                  Center(
-                                      child: Container(
-                                        width: 171.0,   // Equivalent to 40 mm
-                                        height: 209.0,  // Equivalent to 50 mm
-                                        decoration: BoxDecoration(
-                                          border: Border.all(color: Colors.black,width: 3),
-                                          image: DecorationImage(
-                                            image: AssetImage('${widget.ppialumnimodel!.img}'),
-                                            fit: BoxFit.fill,
-                                          ),
-                                          //shape: BoxShape.circle,
-                                        ),
-                                      )
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        SizedBox(height: 10.h,),
+                                        Center(child: Text('${widget.ppialumnimodel!.name}',style: largeblack,)),
+                                        SizedBox(height: 10.h,),
+                                        Card(
+                                          color: AppColor.ppialumniview,
+                                            child: Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: ListTile(
+                                                leading: Container(
+                                                  padding: EdgeInsets.all(8.0), // আইকনের চারপাশে স্পেস
+                                                  decoration: BoxDecoration(
+                                                    shape: BoxShape.circle,
+                                                    border: Border.all(
+                                                      color: Colors.white, // বর্ডারের রঙ
+                                                      width: 3.0,         // বর্ডারের প্রস্থ
+                                                    ),
+                                                    color: Colors.transparent,   // পটভূমির রঙ (আইকনের পিছনে)
+                                                  ),
+                                                  child: Icon(
+                                                    Icons.business,
+                                                    color: Colors.blue.withOpacity(0.7), // আইকনের রঙ
+                                                    size: 25,            // আইকনের আকার
+                                                  ),
+                                                ),
+                                                title: Text('${widget.ppialumnimodel!.company}',style: mediumblack,),
+                                              ),
+                                            )),
+                                        SizedBox(height: 10.h,),
+                                        Card(
+                                            color: AppColor.ppialumniview,
+                                            child: Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: ListTile(
+                                                onTap: _launchDialPad, // ফোন নম্বরে ক্লিক করলে ডায়ালার খোলে
+                                                leading: Container(
+                                                  padding: EdgeInsets.all(8.0), // আইকনের চারপাশে স্পেস
+                                                  decoration: BoxDecoration(
+                                                    shape: BoxShape.circle,
+                                                    border: Border.all(
+                                                      color: Colors.white, // বর্ডারের রঙ
+                                                      width: 3.0,         // বর্ডারের প্রস্থ
+                                                    ),
+                                                    color: Colors.transparent,   // পটভূমির রঙ (আইকনের পিছনে)
+                                                  ),
+                                                  child: Icon(
+                                                    Icons.phone,
+                                                    color: Colors.blue.withOpacity(0.7), // আইকনের রঙ
+                                                    size: 25,            // আইকনের আকার
+                                                  ),
+                                                ),
+                                                title: Text('${widget.ppialumnimodel!.phone}',style: mediumblack,),
+                                              ),
+                                            )),
+                                        SizedBox(height: 10.h,),
+                                        Card(
+                                            color: AppColor.ppialumniview,
+                                            child: Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: ListTile(
+                                                onTap: _sendEmail,
+                                                leading: Container(
+                                                  padding: EdgeInsets.all(8.0), // আইকনের চারপাশে স্পেস
+                                                  decoration: BoxDecoration(
+                                                    shape: BoxShape.circle,
+                                                    border: Border.all(
+                                                      color: Colors.white, // বর্ডারের রঙ
+                                                      width: 3.0,         // বর্ডারের প্রস্থ
+                                                    ),
+                                                    color: Colors.transparent,   // পটভূমির রঙ (আইকনের পিছনে)
+                                                  ),
+                                                  child: Icon(
+                                                    Icons.email,
+                                                    color: Colors.blue.withOpacity(0.7), // আইকনের রঙ
+                                                    size: 25,            // আইকনের আকার
+                                                  ),
+                                                ),
+                                                title: Text('${widget.ppialumnimodel!.email}',style: mediumblack,),
+                                              ),
+                                            )),
+                                        SizedBox(height: 10.h,),
+                                        Card(
+                                            color: AppColor.ppialumniview,
+                                            child: Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: ListTile(
+                                                leading: Container(
+                                                  padding: EdgeInsets.all(8.0), // আইকনের চারপাশে স্পেস
+                                                  decoration: BoxDecoration(
+                                                    shape: BoxShape.circle,
+                                                    border: Border.all(
+                                                      color: Colors.white, // বর্ডারের রঙ
+                                                      width: 3.0,         // বর্ডারের প্রস্থ
+                                                    ),
+                                                    color: Colors.transparent,   // পটভূমির রঙ (আইকনের পিছনে)
+                                                  ),
+                                                  child: Icon(
+                                                    Icons.group,
+                                                    color: Colors.blue.withOpacity(0.7), // আইকনের রঙ
+                                                    size: 25,            // আইকনের আকার
+                                                  ),
+                                                ),
+                                                title: Text('${widget.ppialumnimodel!.department}',style: mediumblack,),
+                                              ),
+                                            )),
+                                        SizedBox(height: 10.h,),
+                                        Card(
+                                            color: AppColor.ppialumniview,
+                                            child: Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: ListTile(
+                                                leading: Container(
+                                                  padding: EdgeInsets.all(8.0), // আইকনের চারপাশে স্পেস
+                                                  decoration: BoxDecoration(
+                                                    shape: BoxShape.circle,
+                                                    border: Border.all(
+                                                      color: Colors.white, // বর্ডারের রঙ
+                                                      width: 3.0,         // বর্ডারের প্রস্থ
+                                                    ),
+                                                    color: Colors.transparent,   // পটভূমির রঙ (আইকনের পিছনে)
+                                                  ),
+                                                  child: Icon(
+                                                    Icons.format_list_numbered,
+                                                    color: Colors.blue.withOpacity(0.7), // আইকনের রঙ
+                                                    size: 25,            // আইকনের আকার
+                                                  ),
+                                                ),
+                                                title: Text('${widget.ppialumnimodel!.roll}',style: mediumblack,),
+                                              ),
+                                            )),
+                                        SizedBox(height: 10.h,),
+                                        Card(
+                                            color: AppColor.ppialumniview,
+                                            child: Padding(
+                                              padding: const EdgeInsets.all(8.0),
+                                              child: ListTile(
+                                                leading: Container(
+                                                  padding: EdgeInsets.all(8.0), // আইকনের চারপাশে স্পেস
+                                                  decoration: BoxDecoration(
+                                                    shape: BoxShape.circle,
+                                                    border: Border.all(
+                                                      color: Colors.white, // বর্ডারের রঙ
+                                                      width: 3.0,         // বর্ডারের প্রস্থ
+                                                    ),
+                                                    color: Colors.transparent,   // পটভূমির রঙ (আইকনের পিছনে)
+                                                  ),
+                                                  child: Icon(
+                                                    Icons.grade,
+                                                    color: Colors.blue.withOpacity(0.7), // আইকনের রঙ
+                                                    size: 25,            // আইকনের আকার
+                                                  ),
+                                                ),
+                                                title: Text('${widget.ppialumnimodel!.cgpa}',style: mediumblack,),
+                                              ),
+                                            )),
+                                        SizedBox(height: 10.h,),
+                                      ],
+                                    ),
                                   ),
-                                  SizedBox(height: 10.h,),
-                                  Center(child: Text('${widget.ppialumnimodel!.name}',style: largeblack,)),
-                                  SizedBox(height: 10.h,),
-                                  Text('${widget.ppialumnimodel!.company}',style: largeblack,),
-                                  SizedBox(height: 10.h,),
-                                  Text('${widget.ppialumnimodel!.phone}',style: largeblack,),
-                                  SizedBox(height: 10.h,),
-                                  Text('${widget.ppialumnimodel!.email}',style: largeblack,),
-                                  SizedBox(height: 10.h,),
-                                  Text('${widget.ppialumnimodel!.department}',style: largeblack,),
-                                  SizedBox(height: 10.h,),
-                                  Text('${widget.ppialumnimodel!.roll}',style: largeblack,),
-                                  SizedBox(height: 10.h,),
-                                  Text('${widget.ppialumnimodel!.cgpa}',style: largeblack,),
-                                  SizedBox(height: 10.h,),
+
                                 ],
                               ),
-                            ),
+                              SizedBox(height: 10.h,),
 
-                          ],
+                            ],
+                          ),
                         ),
-                        SizedBox(height: 10.h,),
+                      ),
 
-                      ],
+                    ),
+                  Positioned(
+                    top: -MediaQuery.of(context).size.height * 0.1, // স্ক্রিনের উচ্চতার ১০% হিসাবে উপরে থাকবে
+                    right: MediaQuery.of(context).size.width * 0.3,  // স্ক্রিনের প্রস্থের ৩০% হিসাবে ডানে অবস্থান করবে
+                    child: Container(
+                      padding: EdgeInsets.all(2.0), // বর্ডার ও ছবির মধ্যে কিছু জায়গা রাখবে
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.grey, // বর্ডারের রঙ
+                          width: 5.0, // বর্ডারের প্রস্থ
+                        ),
+                      ),
+                      child: CircleAvatar(
+                        radius: MediaQuery.of(context).size.width * 0.2 - 4, // প্যাডিং এর জন্য একটু ছোট করে দিবে
+                        backgroundImage: AssetImage('${widget.ppialumnimodel!.img}'),
+                      ),
                     ),
                   ),
-                ),
+
+
+                ],
               ),
             ],
           ),
